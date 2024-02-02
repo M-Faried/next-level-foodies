87746b9f7a867dfedb83f1fc574c8f91cca75c18
m-faried <m.a.faried@gmail.com>
2024-02-02 20:20:11 +0200

    Adding the basic style and excercise 1

---
+++ app/meals/[mealsSlug]/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MealDetailsPage = ({ params }) => {
+  console.log("Here are the slug params", params);
+  return <div>Meal Details</div>;
+};
+
+export default MealDetailsPage;
---
+++ app/meals/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MealsPage = () => {
+  return <div>Here is the meals page</div>;
+};
+
+export default MealsPage;
