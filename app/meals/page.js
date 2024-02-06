c3dd7441886bab8ce0f9843aac5d93a1ac5ea7fa
m-faried <m.a.faried@gmail.com>
2024-02-06 23:38:19 +0200

    Using suspense and fallback for better localiaed loading.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,23 @@
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import Link from "next/link";
+import { Suspense } from "react";
 import styles from "./page.module.css";
 
-const MealsPage = async () => {
-  const meals = await getMeals();
+// You can either use "loading" reserved component name, or use suspense
+// for better user experience and control on which part to display the
+// the loading spinner at.
 
+const Meals = async () => {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
+
+const LoadingFallback = () => {
+  return <h1 className={styles.loading}>Fetching Meals...</h1>;
+};
+
+const MealsPage = async () => {
   return (
     <>
       <header className={styles.header}>
@@ -21,7 +33,9 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<LoadingFallback />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
