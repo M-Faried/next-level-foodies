a173700d2ae57a9e714c735961d7c34fbec9467c
m-faried <m.a.faried@gmail.com>
2024-02-06 20:07:04 +0200

    Separating the header background into a dedicated component.

--- components/main-header.js
+++ components/main-header.js
@@ -1,27 +1,30 @@
-import React from "react";
-import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import Image from "next/image";
+import Link from "next/link";
+import MainHeaderBackground from "./main-header-background";
 import styles from "./main-header.module.css";
 
 const MainHeader = () => {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority />
-        Next Level Food
-      </Link>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Next Level Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 export default MainHeader;
