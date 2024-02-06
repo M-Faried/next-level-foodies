831c214b1dfcd8402d647f54b2a7a304db0d454a
m-faried <m.a.faried@gmail.com>
2024-02-06 23:01:11 +0200

    Removing layout, and updating the meals page to display the header and an empty meals grid.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,27 @@
-import React from "react";
+import MealsGrid from "@/components/meals/meals-grid";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 const MealsPage = () => {
-  return <div>Here is the meals page</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
