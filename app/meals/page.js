2ab1bd8d286c2d87d6e02fcb32d6d65256239762
m-faried <m.a.faried@gmail.com>
2024-02-12 19:41:47 +0200

    Adding different metadata to the meals page.

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,9 +5,16 @@ import Link from "next/link";
 import { Suspense } from "react";
 import styles from "./page.module.css";
 
-// You can either use "loading" reserved component name, or use suspense
+// Note: You can either use "loading" reserved component name, or use suspense
 // for better user experience and control on which part to display the
 // the loading spinner at.
+
+// Note: The following is different than the entire website. Each page can have
+// its own metadata.
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by other contributors.",
+};
 
 const Meals = async () => {
   const meals = await getMeals();
